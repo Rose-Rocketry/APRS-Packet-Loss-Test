7c3d3cf8136f205b2d5ea040bcb31fc85ddc7ea0
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-10-17 18:01:03 -0400

    Use python KISS library

--- recieve.py
+++ recieve.py
@@ -1,13 +1,16 @@
-from subprocess import Popen, PIPE
+from subprocess import DEVNULL, Popen, PIPE
+from time import sleep
+from kiss import TCPKISS
 
 all_procs: list[Popen] = []
 
 PACKET_LOOP_COUNT = 100
 PACKET_ANALYSIS_LENGTH = 100
 
+
 def main():
     rtl_fm = Popen(
-        ["rtl_fm", "-f", "145.15M", "-"],
+        ["rtl_fm", "-f", "145.00M", "-"],
         stdout=PIPE,
     )
     all_procs.append(rtl_fm)
@@ -17,37 +20,44 @@ def main():
         "-"
     ],
                      stdin=rtl_fm.stdout,
-                     stdout=PIPE)
+                     stdout=DEVNULL)
 
-    was_lost = []
-    last_id = None
+    sleep(5)  # Wait for direwolf to start
 
-    for line in direwolf.stdout:
-        # print("[DW] ", line[:-1].decode())
-        if b"WB2OSZ-15>TEST:XX4XXX C3 A1 D4 C3 F6 C3 F6 B2 B2 C3#" in line:
-            id = int(line[-9:-7], 16)
-            if last_id is None:
-                last_id = id
-                was_lost.append(False)
-                print("First packet received")
-                continue
-
-            skipped = (id - (last_id + 1) + PACKET_LOOP_COUNT) % PACKET_LOOP_COUNT
-            last_id = id
-            was_lost += ([True] * skipped)
-            was_lost.append(False)
-
-            if len(was_lost) > PACKET_ANALYSIS_LENGTH:
-                was_lost = was_lost[-PACKET_ANALYSIS_LENGTH:]
-
-            mapped = "".join(map(lambda x: "_" if x else "X", was_lost))
-
-            lost_count = was_lost.count(True)
-            print(f"#{id:02d} Lost {lost_count:<3}of last {len(was_lost):<3} ({round(lost_count / len(was_lost) * 100):>2}% loss) [{mapped}]", end="\r")
+    k = TCPKISS("127.0.0.1", 8001)
+    k.start()
+    k.read(callback=callback)
 
 
-def callback(x):
-    print(repr(x))
+was_lost = []
+last_id = None
+
+
+def callback(packet: bytes):
+    global was_lost, last_id
+
+
+    id = int(packet[-3:-1], 16)
+    if last_id is None:
+        print("First packet received")
+        last_id = id
+        return
+
+    skipped = (id -
+                (last_id + 1) + PACKET_LOOP_COUNT) % PACKET_LOOP_COUNT
+    last_id = id
+    was_lost += ([True] * skipped)
+    was_lost.append(False)
+
+    if len(was_lost) > PACKET_ANALYSIS_LENGTH:
+        was_lost = was_lost[-PACKET_ANALYSIS_LENGTH:]
+
+    mapped = "".join(map(lambda x: "_" if x else "X", was_lost))
+
+    lost_count = was_lost.count(True)
+    print(
+        f"#{id:02d} Lost {lost_count:<3}of last {len(was_lost):<3} ({round(lost_count / len(was_lost) * 100):>2}% loss) [{mapped}]",
+        end="\r")
 
 
 if __name__ == "__main__":
